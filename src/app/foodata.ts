cd99e5329d099fd278d83b7026350944a21e9ae2
Berit Alasmaki <berit.alasmaki@leadin.fi>
2018-03-18 18:55:48 +0200

    Foodata.ts hakusanojen päivitys

--- src/app/foodata.ts
+++ src/app/foodata.ts
@@ -1,17 +1,17 @@
 import { Element } from './interface';
 
 export const data: Element[] = [
-  {name: 'Apple', calories: 1.0079, keyword: 'H'},
-  {name: 'Cotton Cheese', calories: 4.0026, keyword: 'He'},
-  {name: 'Chicken Breast', calories: 6.941, keyword: 'Li'},
-  {name: 'Avocado', calories: 9.0122, keyword: 'Be'},
-  {name: 'Banana', calories: 10.811, keyword: 'B'},
-  {name: 'Cashew Nuts', calories: 12.0107, keyword: 'C'},
-  {name: 'Grapes', calories: 14.0067, keyword: 'N'},
-  {name: 'Tomatoes', calories: 15.9994, keyword: 'O'},
-  {name: 'Oat Meals', calories: 18.9984, keyword: 'F'},
-  {name: 'Berry Smoothie', calories: 20.1797, keyword: 'Ne'},
-  {name: 'Lemon Water', calories: 22.9897, keyword: 'Na'},
-  {name: 'Magnesium', calories: 24.305, keyword: 'Mg'},
+  {name: 'Apple', calories: 1.0079, keyword: 'Chocolate'},
+  {name: 'Cotton Cheese', calories: 4.0026, keyword: 'Cheese'},
+  {name: 'Chicken Breast', calories: 6.941, keyword: 'Pork'},
+  {name: 'Avocado', calories: 9.0122, keyword: 'Chocolate'},
+  {name: 'Banana', calories: 10.811, keyword: 'Chocolate'},
+  {name: 'Cashew Nuts', calories: 12.0107, keyword: 'Chips'},
+  {name: 'Grapes', calories: 14.0067, keyword: 'Candy'},
+  {name: 'Tomatoes', calories: 15.9994, keyword: 'Ketchup'},
+  {name: 'Oat Meals', calories: 18.9984, keyword: 'Corn Flakes'},
+  {name: 'Berry Smoothie', calories: 20.1797, keyword: 'Ice cream'},
+  {name: 'Lemon Water', calories: 22.9897, keyword: 'Lemonade'},
+  {name: 'Sweet potatoes', calories: 24.305, keyword: 'French fries'},
 
 ];
