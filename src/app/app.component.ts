436ee83be1be07200f660e6573ce3dd05b31d4b6
Berit Alasmaki <berit.alasmaki@leadin.fi>
2018-03-18 23:17:42 +0200

    Muutokset main.componentissa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {Sort} from '@angular/material';
 import {FormControl} from '@angular/forms';
-import {data} from './foodata';
+
 
 @Component({
   selector: 'app-root',
@@ -9,11 +9,5 @@ import {data} from './foodata';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  displayedColumns = ['name', 'calories']; // 'keyword' jätetty pois, koska hakukone käyttää sitä vain löytäkseen sopivan korvikkeen.
-  dataSource = data;
-  query: string = '';
 
-  getData() {
-    return this.dataSource.filter(item => item.keyword.toLowerCase().includes(this.query.toLowerCase()));
-  }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {data} from './foodata';
 
 @Component({
   selector: 'app-main',
@@ -10,6 +11,14 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  displayedColumns = ['name', 'calories']; // 'keyword' jätetty pois, koska hakukone käyttää sitä vain löytäkseen sopivan korvikkeen.
+  dataSource = data;
+  query: string = '';
+
+  getData() {
+    return this.dataSource.filter(item => item.keyword.toLowerCase().includes(this.query.toLowerCase()));
   }
 
 }
